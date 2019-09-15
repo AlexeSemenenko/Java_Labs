60d544f2ab2a5f20168efc9c4684ad4c392588af
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-09-15 17:01:32 +0300

    Add files via upload

--- Lab1_Teylor/Main.java
+++ Lab1_Teylor/Main.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        int k;
+        double x;
+        Scanner in = new Scanner(System.in);
+
+        try{
+            System.out.println("Input x: ");
+            x = in.nextDouble();
+            System.out.println("Input k: ");
+            k = in.nextInt();
+        }
+        catch (Throwable except) {
+            System.out.println("Something went wrong");
+            return;
+        }
+
+        System.out.printf("My calculation: %.3f \nJava sin: %.3f",
+                Teylor_Calc.calculate(x, k), Math.sin(x));
+    }
+}
