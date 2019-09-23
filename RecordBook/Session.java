3f61149ab5e5982d28459072049120dfd12a5baa
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-09-23 13:09:32 +0300

    Add files via upload
    delete enum from Session.java and make new class Subject

--- RecordBook/Session.java
+++ RecordBook/Session.java
@@ -3,12 +3,5 @@ package com.company;
 import java.util.Map;
 
 public class Session {
-    public Map<Subjects, Integer> subjects;
-}
-
-enum Subjects{
-    CALCULUS,
-    LINEAR_ALGEBRA,
-    COMPUTER_SCIENCE,
-    ENGLISH
+    public Map<Subject, Integer> subjects;
 }
---
+++ RecordBook/Subject.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public enum Subject{
+    CALCULUS,
+    LINEAR_ALGEBRA,
+    COMPUTER_SCIENCE,
+    ENGLISH,
+    HISTORY
+}
