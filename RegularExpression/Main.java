e21d0c3379f55c9ddd8202cc3d4673c4cab819e0
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-11-03 15:04:55 +0300

    Add files via upload

--- RegularExpression/Main.java
+++ RegularExpression/Main.java
@@ -1,1 +1,39 @@
+package com.company;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("input.txt");
+        Scanner sc = new Scanner(fr);
+
+        String str = sc.nextLine();
+        fr.close();
+
+        Pattern pattern = Pattern.compile("\\(.*?\\)");
+        Matcher matcher = pattern.matcher(str);
+
+        List<String> parts = new ArrayList<>();
+
+        while(matcher.find()){
+            parts.add(str.substring(matcher.start(), matcher.end()));
+        }
+
+        for(String temp : parts){
+            str = str.replaceAll("\\(" + temp + "\\)", "");
+        }
+
+        FileWriter fw = new FileWriter("output.txt");
+
+        fw.write(str);
+        fw.close();
+    }
+}
